2e2dc5b020d2b3df8fe882c53d0829de6344c77d
LaurenArica <aguerrerolauren@crece.uss.edu.pe>
2023-10-13 19:38:50 -0500

    Quiz

---
+++ data/data.js
@@ -0,0 +1,52 @@
+export const data = [
+    {
+      question: "¿Cuántas zonas horarias tiene en Rusia?",
+      choices: ["5", "9", "11", "15"],
+      answer: "11",
+    },
+    {
+      question: "¿Cuál es la flor nacional de Japón?",
+      choices: ["La Kantuta", "Cerezo", "Flor de Cerezo", "Margarita"],
+      answer: "Flor de Cerezo",
+    },
+    {
+      question: "¿Cuál es el animal nacional de Australia?",
+      choices: ["Koala", "Canguro", "Paloma", "Vibora"],
+      answer: "Canguro",
+    },
+    {
+      question: "¿Cuántos días le toma a la tierra dar una vuelta a la órbita del sol?",
+      choices: ["365", "364", "366", "200"],
+      answer: "365",
+    },
+    {
+      question: "¿Cuál de los siguientes imperios no tenía un idioma escrito?",
+      choices: ["Inca", "Aztecas", "Egipcios", "Romanos"],
+      answer: "Inca",
+    },
+    {
+      question: "¿Cuál es el país más pequeño del mundo?",
+      choices: ["Portugal", "Rusia", "Surinam", "El Vaticano"],
+      answer: "El Vaticano",
+    },
+    {
+      question: "¿De qué ciudad son originarios los Beatles?",
+      choices: ["Madrid", "Paris", "Liverpool", "Arsenal"],
+      answer: "Liverpool",
+    },
+    {
+      question: "¿Qué equipo de fútbol se le conoce como ‘Garra crema’'?",
+      choices: ["Universitario", "Alianza Lima", "Sporting Cristal", "Juan Aurich"],
+      answer: "Universitario",
+    },
+    {
+      question: "¿Quién inventó la World Wide Web, y cuándo?",
+      choices: ["Vinton Cerf y Robert Kahn, 1999", "Elon Musk, 2000", "Bill Gates, 1985", "Tim Berners-Lee, 1990"],
+      answer: "Tim Berners-Lee, 1990",
+    },
+    {
+      question: "¿Cuál es la capital de Canadá?",
+      choices: ["Los Angeles", "Ottawa", "Vancouver", "Toronto"],
+      answer: "Ottawa",
+    },
+  ];
